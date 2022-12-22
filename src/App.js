adf4639dd01929f3023fb2e592234b2b4f2159dd
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-12-22 14:23:34 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import logo from "./main.png"
+
+
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container mx-auto flex m-8">
+
+      <div className='box m-4'>
+
+      <h2 className="font-serif text-4xl font-semibold"> Welcome to Aibacus </h2>
+      <p className='mt-6 font-serif text-l'>A place where you can:</p> 
+      <ul className='font-serif text-l p-2 list-disc'>
+        <li>help your child learn faster and more effectively</li>
+        <li>instantly create assignments adapted to your child's unique learning style</li>
+        <li>observe the pace and analyze the learning progress</li>
+        <li>share your ideas with other parents</li>
+      </ul>
+
+      <button className='mt-6 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl'>
+        Sign up today
+      </button>
+      </div>
+
+     
+
+      <div className='ml-6 flex' >
+      <img src={logo} width="400" className='rounded-xl'></img>
+      </div>
+
+      </div>
     </div>
   );
 }
