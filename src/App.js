41279c4f0846608db9fc4ca5b4838a845611513e
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-12-25 22:45:06 -0800

    added some pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import { useState } from 'react';
-import logo from "./main.png"
-
+import homepic from "./main.png"
+import Footer from './components/Footer';
+import { Link } from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
 
-  
 
   return (
-    <div>
+    <Router>
+    <>
       <div className="container mx-auto flex m-8 place-content-center">
-
       <div className='box m-4'>
-
-      <h2 className="font-serif text-4xl font-semibold"> Welcome to Aibacus </h2>
-
-      <div className='sm:hidden md:hidden lg:hidden xl:hidden max-w-xs mx-auto m-4' >
-      <img src={logo} className='rounded-xl'></img>
+        <h2 className="font-serif text-4xl font-semibold"> Welcome to Aibacus </h2>
+        <div className='sm:hidden md:hidden lg:hidden xl:hidden max-w-xs mx-auto m-4' >
+        <img src={homepic} className='rounded-xl'></img>
       </div>
 
       <p className='mt-6 font-serif text-l'>A place where you can:</p> 
@@ -31,17 +30,21 @@ function App() {
       <button className='mt-6 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl'>
         Sign up today
       </button>
+      
+      <p className='mt-6 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl'>
+      <Link to="/Footer">
+      Next page
+      </Link>
+      </p>
+
       </div>
 
-     
       <div className='hidden sm:block md:block lg:block xl:block mr-6 max-w-md' >
-      <img src={logo} className='rounded-xl'></img>
+      <img src={homepic} className='rounded-xl'></img>
       </div>
-     
-
       </div>
-
-    </div>
+    </>
+    </Router>
   );
 }
 
